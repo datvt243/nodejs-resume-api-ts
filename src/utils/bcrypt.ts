b4567a4c8b66f11c4155c42adc44121180ad27a6
_david <votan.it@gmail.com>
2025-02-09 16:39:34 +0700

    update bcrypt and test

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -6,16 +6,25 @@
 
 import bcrypt from 'bcrypt';
 
-const saltRounds = 10;
+const saltRounds = 10; // số vòng mã hóa
 
-export const bcryptGenerateSalt = (pass: string) => {
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hash = bcrypt.hashSync(pass, salt);
-    return hash || pass;
+export const bcryptGenerateSalt = async (pass: string): Promise<string> => {
+    try {
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(pass, salt);
+        return hash;
+    } catch (e) {
+        console.error('Error hashing password:', e);
+        throw new Error('Failed to hash password');
+    }
 };
 
 export const bcryptCompareHash = async (str: string, hash: string): Promise<boolean> => {
     if (!str || !hash) return false;
-    const match = await bcrypt.compare(str, hash);
-    return match;
+    try {
+        return Boolean(await bcrypt.compare(str, hash)); // make sure alway return boolean
+    } catch (error) {
+        console.error('Error comparing hash:', error);
+        return false;
+    }
 };
