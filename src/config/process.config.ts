723c0032e0b66840d114a75f36c2f92f056c0bee
_david <votan.it@gmail.com>
2026-03-07 18:17:04 +0700

    #32 - changed URL mongo

--- src/config/process.config.ts
+++ src/config/process.config.ts
@@ -9,6 +9,7 @@ import 'dotenv/config';
 const ENV_KEYS = [
     'NODE_ENV',
     'LOCAL_PORT',
+    'MONGO_URI',
     'MONGOBD_USER',
     'MONGOBD_PASSWORD',
     'SESSION_SECRET',
@@ -22,7 +23,26 @@ for (const k of ENV_KEYS) {
     result[k] = process.env?.[k];
 }
 
-const { NODE_ENV, LOCAL_PORT, MONGOBD_USER, MONGOBD_PASSWORD, SESSION_SECRET, TOKEN_SECRET, TOKEN_REFRESH, TOKEN_EXP_IN } =
-    process.env;
+const {
+    NODE_ENV,
+    LOCAL_PORT,
+    MONGO_URI,
+    MONGOBD_USER,
+    MONGOBD_PASSWORD,
+    SESSION_SECRET,
+    TOKEN_SECRET,
+    TOKEN_REFRESH,
+    TOKEN_EXP_IN,
+} = process.env;
 
-export { NODE_ENV, LOCAL_PORT, MONGOBD_USER, MONGOBD_PASSWORD, SESSION_SECRET, TOKEN_SECRET, TOKEN_REFRESH, TOKEN_EXP_IN };
+export {
+    NODE_ENV,
+    LOCAL_PORT,
+    MONGO_URI,
+    MONGOBD_USER,
+    MONGOBD_PASSWORD,
+    SESSION_SECRET,
+    TOKEN_SECRET,
+    TOKEN_REFRESH,
+    TOKEN_EXP_IN,
+};
